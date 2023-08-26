8ba07bb2eae80ed7bcf2bd86ffb0e5f1316bf194
seungheyon <71931476+seungheyon@users.noreply.github.com>
2023-08-26 17:15:47 +0900

    [unrated] Title: 옹알이 (2), Time: 0.11 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.java
+++ 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.java
@@ -1,24 +1,23 @@
+import java.util.HashSet;
+import java.util.Set;
+
 class Solution {
     public int solution(String[] babbling) {
         int answer = 0;
         String[] babbleWords = {"aya", "ye", "woo", "ma"};
-        
+
         for(String word : babbling){
             String beforebabble = "";
             while (true){
                 boolean match = false;
-                
                 for(String babble : babbleWords){
                     if(word.startsWith(babble)){
                         if(babble.equals(beforebabble)){
-                            System.out.println(word);
-                            System.out.println(babble+"::"+beforebabble);
                             break;
                         }
                         beforebabble = babble;
                         match = true;
                         word = word.substring(babble.length());
-                        System.out.println(word);
                         if(word.equals("")){
                             break;
                         }
@@ -32,7 +31,6 @@ class Solution {
                     break;
                 }
             }
-            System.out.println(answer);
         }
 
         return answer;
