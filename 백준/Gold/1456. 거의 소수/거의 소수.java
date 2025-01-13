c037f7b7016476be183c75daa93a1bfadce203d0
seungheyon <71931476+seungheyon@users.noreply.github.com>
2025-01-14 03:26:28 +0900

    [Gold V] Title: 거의 소수, Time: 252 ms, Memory: 44456 KB -BaekjoonHub

--- 백준/Gold/1456. 거의 소수/거의 소수.java
+++ 백준/Gold/1456. 거의 소수/거의 소수.java
@@ -11,29 +11,18 @@ public class Main {
         //int n = Integer.parseInt(br.readLine());
         //int k = Integer.parseInt(str[1]);
         String[] str = br.readLine().split(" ");
-        double m = Double.parseDouble(str[0]);
-        double n = Double.parseDouble(str[1]);
+        long m = Long.parseLong(str[0]);
+        long n = Long.parseLong(str[1]);
         int num = (int)Math.sqrt(n);
 
         List<Integer> primes = eratosthenes_che(num);
-        int count = 0;
-
-        // Count "almost primes" within range [a, b]
-        for (int prime : primes) {
-            long power = (long) prime * prime; // Start with p^2
-            while (power <= n) {
-                if (power >= m) {
-                    count++;
-                }
-                // Avoid overflow when calculating the next power
-                if (power > n / prime) {
-                    break;
-                }
-                power *= prime; // Move to the next power
-            }
+        int sum = 0;
+        for (Integer prime : primes) {
+            int asd = greaterPower(prime,m, n);
+            //System.out.println(asd);
+            sum += asd;
         }
-
-        System.out.println(count);
+        System.out.println(sum);
     }
     
     private static List<Integer> eratosthenes_che(int number){
@@ -59,23 +48,18 @@ public class Main {
         return result;
     }
 
-    private static int greaterPower(int n, double a, double b){
-        double logN = Math.log(n);  // Precompute log(n)
-        int kMax = (int) (Math.log(b) / logN); // Maximum exponent
-        int kMin = (int) (Math.log(a) / logN);  // Minimum exponent
-        if(a>1){
-            kMin = (int) (Math.log(a-1) / logN);  
+    private static int greaterPower(int n, long a, long b){
+        long power = (long)n*n;
+        int cnt = 0;
+        while(power<=b){
+            if(power>=a){
+                cnt++;
+            }
+            if(power>b/n){
+                return cnt;
+            }
+            power *=n;
         }
-        if(kMax>0){
-            kMax--;
-        }
-        if(kMin>0){
-            kMin--;
-        }
-
-        //System.out.println("when prime = "+n+", a : "+a+", b : "+b+" // log(b) : "+kMax+", log(a) : "+kMin);
-
-        // Return the count of valid powers
-        return  kMax - kMin;
+        return cnt;
     }
 }
