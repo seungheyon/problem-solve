9bfb426db0fa5a0c10181f29758176ebcc582bcf
seungheyon <71931476+seungheyon@users.noreply.github.com>
2025-10-30 18:32:43 +0900

    [level 2] Title: 타겟 넘버, Time: 5.17 ms, Memory: 79.5 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,18 +1,19 @@
 class Solution {
     int answer = 0;
     public int solution(int[] numbers, int target) {
-        search(0, numbers, target, 0);
+        
+        dfs(numbers, target, 0, 0);
         return answer;
     }
     
-    private void search(int i, int[] numbers, int target, int cur){
-        if(i==numbers.length){
-            if(cur==target){
+    private void dfs(int[] numbers, int target, int sum, int cur){
+        if(cur==numbers.length){
+            if(sum==target){
                 answer++;
             }
             return;
         }
-        search(i+1, numbers, target, cur+numbers[i]);
-        search(i+1, numbers, target, cur-numbers[i]);
+        dfs(numbers, target, sum-numbers[cur], cur+1);
+        dfs(numbers, target, sum+numbers[cur], cur+1);
     }
 }
