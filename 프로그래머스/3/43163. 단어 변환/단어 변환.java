563d4aadce67532a5d0856bd2077cc9ea21f50d0
seungheyon <71931476+seungheyon@users.noreply.github.com>
2025-10-30 21:25:49 +0900

    [level 3] Title: 단어 변환, Time: 0.35 ms, Memory: 83.4 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,61 +1,47 @@
 import java.util.*;
 class Solution {
     public int solution(String begin, String target, String[] words) {
-        int answer = 0;
-        List<String> wordList = new ArrayList<>();
+        int answer = 1;
+        
         Queue<String> que = new LinkedList<>();
-        Set<String> v = new HashSet<>();
-        
-        for(String word : words){
-            wordList.add(word);
-        }
-        
         que.add(begin);
+        Set<String> vSet = new HashSet<>();
+        vSet.add(begin);
         
         while(!que.isEmpty()){
             int s = que.size();
+            //System.out.println();
+            //System.out.print("in que : ");
             for(int i=0;i<s;i++){
-                if(findTarget(wordList, que, v, target)){
-                    //System.out.println("out!!!!!!!!!!!!!!!!!!!!!!!!!!");
-                    return answer;
+                String cur = que.poll();
+                //System.out.print(cur+" ");
+                for(String word : words){
+                    if(isSimilar(cur, word, vSet)){
+                        if(word.equals(target)){
+                            return answer;
+                        }
+                        que.add(word);
+                    }
                 }
             }
             answer++;
+            //System.out.println();
+            //System.out.println(answer);
         }
-        
         return 0;
     }
     
-    private boolean findTarget(List<String> words, Queue<String> que, Set<String> v, String target){
-        String begin = que.poll();
-        if(begin.equals(target)){
+    private boolean isSimilar(String cur, String comparison, Set<String > vSet){
+        int difCount = 0;
+        for(int i=0;i<cur.length();i++){
+            if(cur.charAt(i)!=comparison.charAt(i)){
+                difCount++;
+            }
+        }
+        if(difCount==1&&!vSet.contains(comparison)){
+            vSet.add(comparison);
             return true;
         }
-        for(String word : words){
-            if(v.contains(word)){
-                continue;
-            }
-            int diff = 0;
-            for(int i=0;i<begin.length();i++){
-                if(begin.charAt(i)!=word.charAt(i)){
-                    diff++;
-                }
-                if(diff>1){
-                    break;
-                }
-            }
-            if(diff==1){
-                que.add(word);
-                v.add(word);
-            }
-        }
-        // for(String q : que){
-        //     System.out.print(q);
-        //     System.out.print(" ");
-        // }
-        // System.out.println(" ");
-        
-        
         return false;
     }
 }
