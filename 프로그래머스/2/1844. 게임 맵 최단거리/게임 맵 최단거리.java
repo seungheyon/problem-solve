ade33f9ace2f4d221561dae1c8284617410b2a12
seungheyon <71931476+seungheyon@users.noreply.github.com>
2025-10-30 20:03:56 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 6.41 ms, Memory: 55.6 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,40 +1,38 @@
 import java.util.*;
+
 class Solution {
     public int solution(int[][] maps) {
         int answer = 1;
-        Queue<int[]> que = new LinkedList<>();
         
+        Queue<int[]> que = new LinkedList<>();
+        maps[0][0] = 0;
         que.add(new int[]{0, 0});
         
-        maps[0][0] = 0;
+        //int route = 0;
         
         while(!que.isEmpty()){
-            
             int s = que.size();
-            
             for(int i=0;i<s;i++){
                 int[] cur = que.poll();
-            
                 if(cur[0]==maps.length-1&&cur[1]==maps[0].length-1){
-                    // 도착
                     return answer;
                 }
-                if(cur[0]<maps.length-1&&maps[cur[0]+1][cur[1]]>0){
-                    maps[cur[0]+1][cur[1]] = 0;
-                    que.add(new int[]{cur[0]+1, cur[1]});
-                }
-                if(cur[1]<maps[0].length-1&&maps[cur[0]][cur[1]+1]>0){
-                    maps[cur[0]][cur[1]+1] = 0;
-                    que.add(new int[]{cur[0], cur[1]+1});
-                }
-                if(cur[0]>0&&maps[cur[0]-1][cur[1]]>0){
+                if(cur[0]>0&&maps[cur[0]-1][cur[1]]!=0){ // left
                     maps[cur[0]-1][cur[1]] = 0;
                     que.add(new int[]{cur[0]-1, cur[1]});
                 }
-                if(cur[1]>0&&maps[cur[0]][cur[1]-1]>0){
+                if(cur[1]>0&&maps[cur[0]][cur[1]-1]!=0){ // up
                     maps[cur[0]][cur[1]-1] = 0;
                     que.add(new int[]{cur[0], cur[1]-1});
                 }
+                if(cur[0]+1<maps.length&&maps[cur[0]+1][cur[1]]!=0){ // right
+                    maps[cur[0]+1][cur[1]] = 0;
+                    que.add(new int[]{cur[0]+1, cur[1]});
+                }
+                if(cur[1]+1<maps[0].length&&maps[cur[0]][cur[1]+1]!=0){ // down
+                    maps[cur[0]][cur[1]+1] = 0;
+                    que.add(new int[]{cur[0], cur[1]+1});
+                }  
             }
             answer++;
         }
