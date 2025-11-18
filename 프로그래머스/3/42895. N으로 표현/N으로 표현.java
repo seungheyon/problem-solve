2c08c8754eb83d310dfead6d1899e8a9312168c9
seungheyon <71931476+seungheyon@users.noreply.github.com>
2025-11-18 21:30:43 +0900

    [level 3] Title: N으로 표현, Time: 20.43 ms, Memory: 79.4 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.java
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.java
@@ -1,46 +1,41 @@
 import java.util.*;
 class Solution {
     public int solution(int N, int number) {
-        int answer = 0;
+        int answer = -1;
         
         List<Set<Integer>> dp = new ArrayList<>();
+        Set<Integer> startSet = new HashSet<>();
+        startSet.add(N);
         dp.add(new HashSet<>());
+        dp.add(startSet);
+        if(N==number){
+            return 1;
+        }
         
-        for(int i=1;i<=8;i++){
-            if(i>1){
-                Set<Integer> newSet = new HashSet<>();
-                int nn = N;
-                for(int j=1;j<i;j++){
-                    nn = nn*10 + N;
-                }
-                newSet.add(nn);
-                for(int j=1;j<i;j++){
-                    for(int op1 : dp.get(j)){
-                        for(int op2 : dp.get(i-j)){
-                            newSet.add(op1 + op2);
-                            newSet.add(op1 - op2);
-                            newSet.add(op1 * op2);
-                            if(op2!=0){
-                                newSet.add(op1 / op2);
-                            }
+        for(int i=2;i<=8;i++){
+            Set<Integer> curSet = new HashSet<>();
+            int nn = N;
+            for(int j=1;j<i;j++){
+                nn = nn*10+N;
+            }
+            curSet.add(nn);
+            for(int j=1;j<i;j++){
+                for(int op1 : dp.get(j)){
+                    for(int op2 : dp.get(i-j)){
+                        curSet.add(op1+op2);
+                        curSet.add(op1-op2);
+                        curSet.add(op1*op2);
+                        if(op2!=0){
+                            curSet.add(op1/op2);
                         }
                     }
                 }
-                dp.add(newSet);
             }
-            else{
-                Set<Integer> newSet = new HashSet<>();
-                newSet.add(N);
-                dp.add(newSet);
-            }
-            
-            if(dp.get(i).contains(number)){
+            if(curSet.contains(number)){
                 return i;
             }
+            dp.add(curSet);
         }
-        
-        
-        return -1;
+        return answer;
     }
-    
 }
