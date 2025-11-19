bc12947ea7ba759bc6c750fc6e142670acc7bb61
seungheyon <71931476+seungheyon@users.noreply.github.com>
2025-11-19 16:38:22 +0900

    [level 3] Title: 정수 삼각형, Time: 11.09 ms, Memory: 63.7 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
@@ -1,36 +1,33 @@
-import java.util.*;
 class Solution {
     public int solution(int[][] triangle) {
-        
-        List<int[]> dp = new ArrayList<>();
-        
-        int[] fArr = new int[triangle.length];
-        fArr[0] = triangle[0][0];
-        dp.add(fArr);
-        
-        for(int i=1;i<triangle.length;i++){
-            int[] intArr = new int[triangle.length];
+        int answer = 0;
+        int dp[] = new int[triangle.length];
+        for(int i=0;i<triangle.length;i++){
+            int[] cur = new int[i+1];
             for(int j=0;j<=i;j++){
-                if(j>0&&j<i){
-                    intArr[j] = Math.max(dp.get(i-1)[j-1], dp.get(i-1)[j]) + triangle[i][j];
+                if(j==0){
+                    cur[j]=dp[j]+triangle[i][j];
+                    continue;
+                }
+                if(j==i){
+                    cur[j]=dp[j-1]+triangle[i][j];
+                    continue;
+                }
+                if(dp[j-1]<dp[j]){
+                    cur[j]=dp[j]+triangle[i][j];
                 }
                 else{
-                    if(j==i){
-                        intArr[j] = dp.get(i-1)[j-1] + triangle[i][j];
-                    }
-                    else{
-                        intArr[j] = dp.get(i-1)[j] + triangle[i][j];
-                    }
-                    
+                    cur[j]=dp[j-1]+triangle[i][j];
                 }
             }
-            dp.add(intArr);
+            for(int j=0;j<=i;j++){
+                dp[j] = cur[j];
+            }
         }
-        
         int max = 0;
-        for(int sum : dp.get(dp.size()-1)){
-            if(max<sum){
-                max = sum;
+        for(int i=0;i<dp.length;i++){
+            if(dp[i]>max){
+                max = dp[i];
             }
         }
         
