3ba70525310df88b3b4821c034ab561454d9cd22
seungheyon <71931476+seungheyon@users.noreply.github.com>
2025-12-01 18:36:12 +0900

    [level 4] Title: 징검다리, Time: 36.21 ms, Memory: 80.7 MB -BaekjoonHub

--- 프로그래머스/4/43236. 징검다리/징검다리.java
+++ 프로그래머스/4/43236. 징검다리/징검다리.java
@@ -25,102 +25,10 @@ class Solution {
                     continue;
                 }
                 prev = rocks[i];
-                // 거리의 최솟값(mid)을 기준으로 바위를 제거
-                // if(removed>=n){
-                //     break;
-                // }
-                
-                // before = 1;
-                // after = 1;
-                // while(true){
-                //     if(i-before<0){
-                //         break;
-                //     }
-                //     if(!removed[i-before]){
-                //         break;
-                //     }
-                //     before++;
-                // }
-                // while(true){
-                //     if(i+after==rocks.length){
-                //         break;
-                //     }
-                //     if(!removed[i+after]){
-                //         break;
-                //     }
-                //     after++;
-                // }
-                
-                
-                // if(i-before<=0){
-                //     if(rocks[i]<mid){
-                //         removed[i] = true;
-                //         removeCnt++;
-                //     }
-                // }
-                // else if(i+after==rocks.length){
-                //     if(distance-rocks[i]<mid||rocks[i]-rocks[i-before]<mid){
-                //         removed[i] = true;
-                //         removeCnt++;
-                //     }
-                // }
-                // else{
-                //     if(rocks[i]-rocks[i-before]<mid){
-                //         removed[i] = true;
-                //         removeCnt++;
-                //     }
-                // }
             }
             if(distance-prev<mid){
                 removeCnt++;
             }
-            
-//             int min = distance;
-//             int bef = 0;
-//             for(int i=0;i<rocks.length;i++){
-//                 if(removed[i]){
-//                     bef++;
-//                     continue;
-//                 }
-//                 // before = 1;
-                
-//                 // while(true){
-//                 //     if(i-before<0){
-//                 //         break;
-//                 //     }
-//                 //     if(remove[i-before]){
-//                 //         break;
-//                 //     }
-//                 //     before++;
-//                 // }
-//                 after = 1;
-//                 while(true){
-//                     if(i+after==rocks.length){
-//                         break;
-//                     }
-//                     if(remove[i+after]){
-//                         break;
-//                     }
-//                     after++;
-//                 }
-//                 if(i-bef==0){
-//                     min = rocks[i];
-//                 }
-//                 else if(i+after==rocks.length){
-//                     if(rocks[i]-rocks[i-bef]<min){
-//                         min = rocks[i]-rocks[i-bef];
-//                     }
-//                     if(distance-rocks[i]<min){
-//                         min = distance-rocks[i];
-//                     }
-//                 }
-//                 else{
-//                     if(rocks[i]-rocks[i-bef]<min){
-//                         min = rocks[i]-rocks[i-bef];
-//                     }
-//                 }
-//                 bef = 0;
-//             }
             
             if(removeCnt>n){
                 end = mid-1;
