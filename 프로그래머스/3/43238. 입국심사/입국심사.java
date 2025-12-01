c943209a5139e3a34b001054545aff6cbd374aee
seungheyon <71931476+seungheyon@users.noreply.github.com>
2025-12-01 18:35:12 +0900

    [level 3] Title: 입국심사, Time: 99.44 ms, Memory: 85.5 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -14,35 +14,18 @@ class Solution {
         while(start<=end){
             long mid = (start + end)/2;
             p = 0;
-            
-            
             for(int i=0;i<times.length;i++){
                 p += mid/times[i];
             }
-            
-            // System.out.print("s: ");
-            // System.out.print(start);
-            // System.out.print("  e: ");
-            // System.out.print(end);
-            // System.out.print("  p: ");
-            // System.out.println(p);
-            
             if(p<n){
-                // if(end-start<2){
-                //     answer = end;
-                //     break;
-                // }
                 start = mid+1;
             }
             else{
                 
                 answer = mid;
                 end = mid-1;
-                
             }
         }
-        
-        
         return answer;
     }
 }
